d8596a1e3ee2afc8f7b8a29d252ad12c155df2d8
John Serrano <andrey_201015@hotmail.com>
2023-08-25 17:35:13 -0500

    Changes tsconfig and services

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,12 +1,14 @@
 import express from 'express'
+import { getEntries } from '../services/diariesServices'
 
 const router = express.Router()
 
-router.get('/', (_res, res) => {
-  res.send('Fetching all entry diaries')
+router.get('/', (_req, res) => {
+  const entries = getEntries()
+  res.send(entries)
 })
 
-router.post('/', (_red, res) => {
+router.post('/', (_req, res) => {
   res.send('Saving a diary!')
 })
 
