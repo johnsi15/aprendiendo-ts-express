a40aa71d0ee46efd250b88c22774539520d4b4b3
John Serrano <andrey_201015@hotmail.com>
2023-08-25 17:35:18 -0500

    Changes tsconfig and services#2

---
+++ src/services/diariesServices.ts
@@ -0,0 +1,14 @@
+import { DiaryEntry } from '../types'
+import diaryData from './diaries.json'
+/**
+ Array<DiaryEntry> === DiaryEntry[]
+*/
+// const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
+// se usa as para indicarle a typescript,
+// que la data que viene del json tiene que tratarla con mis types, esto se usa en este caso o en llamadas fetch
+
+export const getEntries = () => diaries
+
+export const addEntry = () => null
+
